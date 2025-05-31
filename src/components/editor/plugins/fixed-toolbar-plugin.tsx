c6f7e3c1fabafa95eab258ea4590aaa724362e3d
malakthec <malakthecatog@gmail.com>
2025-05-31 19:33:43 +0300

    Update fixed-toolbar-plugin.tsx

--- src/components/editor/plugins/fixed-toolbar-plugin.tsx
+++ src/components/editor/plugins/fixed-toolbar-plugin.tsx
@@ -1,17 +1,27 @@
+
 'use client';
 
 import { createPlatePlugin } from '@udecode/plate/react';
 
+import { VersionHistoryProvider } from '@/components/editor/version-history-context';
 import { FixedToolbar } from '@/components/ui/fixed-toolbar';
 import { FixedToolbarButtons } from '@/components/ui/fixed-toolbar-buttons';
+import { VersionHistoryButton } from '@/components/ui/version-history-button';
 
 export const FixedToolbarPlugin = createPlatePlugin({
   key: 'fixed-toolbar',
   render: {
     beforeEditable: () => (
-      <FixedToolbar>
-        <FixedToolbarButtons />
-      </FixedToolbar>
+      <VersionHistoryProvider>
+        <div className="flex flex-col">
+        <div className="flex justify-center py-3 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <VersionHistoryButton />
+          </div>
+          <FixedToolbar>
+            <FixedToolbarButtons />
+          </FixedToolbar>
+        </div>
+      </VersionHistoryProvider>
     ),
   },
 });
